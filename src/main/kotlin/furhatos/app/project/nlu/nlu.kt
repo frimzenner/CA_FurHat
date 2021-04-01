9cb9db2e300f60c29418402d15176afa0cbc6b61
frimzenner <81421589+frimzenner@users.noreply.github.com>
2021-04-01 10:51:52 +0200

    Delete src/main/kotlin/furhatos/app/project directory

--- src/main/kotlin/furhatos/app/project/nlu/nlu.kt
+++
@@ -1,7 +0,0 @@
-package furhatos.app.project.nlu
-
-import furhatos.nlu.*
-import furhatos.nlu.grammar.Grammar
-import furhatos.nlu.kotlin.grammar
-import furhatos.nlu.common.Number
-import furhatos.util.Language
